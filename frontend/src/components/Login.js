70e18414e5c5d41c8eed900916bc3c59a2005ecf
Stephen <stephennnabuike@gmail.com>
2023-03-21 18:38:46 +0100

    Create components and image files

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import { useNavigate } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
+const Login = () => {
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+    Login
+    </div>
+  );
+};
+
+export default Login;
